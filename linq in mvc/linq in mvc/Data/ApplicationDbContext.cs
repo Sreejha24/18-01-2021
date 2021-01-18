882ffe85bcaaeece19dc721b28a4923b5fc0676c
Sreejha24 <saisreejha04@gmail.com>
2021-01-18 19:41:53 +0530

    S

---
+++ linq in mvc/linq in mvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using linq_in_mvc.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace linq_in_mvc.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        
+        public DbSet<linq_in_mvc.Models.Product> Product { get; set; }
+
+       // public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ linq in mvc/linq in mvc/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace linq_in_mvc.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int Qty { get; set; }
+        public decimal Price { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
